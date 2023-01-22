6ee6348cedcfc9c3cffdfe99d39cc9642f442984
AmirTrakic <amirtrakic2020@gmail.com>
2023-01-22 13:37:31 +0100

    .

---
+++ snake_game.py
@@ -0,0 +1,12 @@
+import turtle
+
+#ovo je pocetan screen
+sg = turtle.Screen()
+sg.title("Igra zmi'ce od MENE    (@Ja)")
+sg.bgcolor("green")
+sg.setup(width=1600, height=1600)
+sg.tracer(0)
+
+
+
+sg.mainloop()
